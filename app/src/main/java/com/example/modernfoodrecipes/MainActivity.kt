57453cdb9bbccd53c8822758a5d0dca11ae5d93d
Islam Hassan <eslamwork2019@gmail.com>
2022-03-24 10:55:57 +0200

    the new feature of navigation component

--- app/src/main/java/com/example/modernfoodrecipes/MainActivity.kt
+++ app/src/main/java/com/example/modernfoodrecipes/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.modernfoodrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val navController = findNavController(R.id.navHostFragment)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoritesRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
     }
 }
